762e6d3f1fd6b8373b0ba9140df87d1a3002c623
Azhe-kun <azhe403@gmail.com>
2022-06-10 20:19:41 +0700

    adding HtmlEncode for possibility invalid html markup

---
+++ Serilog.Sinks.TelegramBot/StringHelper.cs
@@ -0,0 +1,12 @@
+﻿using System.Web;
+
+namespace Serilog.Sinks.TelegramBot
+{
+    public static class StringHelper
+    {
+        public static string HtmlEncode(this string htmlStr)
+        {
+            return HttpUtility.HtmlEncode(htmlStr);
+        }
+    }
+}
